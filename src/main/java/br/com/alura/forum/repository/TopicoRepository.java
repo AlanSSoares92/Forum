2785c53a69ac699cc98a9022952162cd31d073ff
AlanSSoares92 <alan_s_soares@outlook.com>
2022-09-14 00:59:57 -0300

    spring boot com get e post criados

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+	//criando metodo findBy + nome do atributo mais recurso
+//	List<Topico> findByCursoNome(String nomeCurso);
+//	
+	//criando metodo da maneira deseja porém tendo que preencher a query - jpqe
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> minhaChamada(@Param("nomeCurso") String nomeCurso);
+
+}
